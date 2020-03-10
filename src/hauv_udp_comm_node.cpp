0aa482e509fd73deff958b95856698ae9dacca2d
Dongha Chung <haetae.dongha@gmail.com>
2020-03-10 19:14:17 +0900

    first commit

---
+++ src/hauv_udp_comm_node.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "hauv_udp_comm_node");
+    ros::NodeHandle nh;
+
+    ROS_INFO("F**K");
+    ros::spin();
+}
